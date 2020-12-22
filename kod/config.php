f2a79afe6fbd074350aaeaefdf2daa7112e6d475
lukaszS21 <64039328+lukaszS21@users.noreply.github.com>
2020-12-22 22:33:49 +0100

    Delete config.php

--- kod/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'fiktfhedevekbo';
-const PASSWORD = '693aa484fac41745629e0e6e3ca34b30771e4f8e5f43f5bdc34fe3d956ff0403';
-const HOST = 'ec2-54-75-246-118.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd7jqj05rpmt77e';
